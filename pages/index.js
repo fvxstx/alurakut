a71aadb3144a152fc2ea688ed517f0cdec2b2b32
Fausto Torres <faut.btorres@gmail.com>
2021-07-14 00:03:10 -0300

    minhas alteracoes 2 aula

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 import MainGrid from "../src/components/MainGrid"
 import Box from "../src/components/Box"
+import {LittleRelationBox} from "../src/components/LittleRelationBox"
+
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSideBar(props){
   return (
@@ -23,17 +26,87 @@ function ProfileSideBar(props){
 }
 
 export default function Home() {
+  const randomUser = 'fvxstx';
+  
   // Mudando o estado de alguma coisa
   // Comunidades: é uma variavel que recebe o primeiro valor
   // setComunidades: ele atribui outro valor pra comunidades a partir de alguma coisa
-  const [comunidades, setComunidades] = React.useState([{
-    id: "131212",
-    title: 'Eu odeio acordar cedo',
-    image: "https://alurakut.vercel.app/capa-comunidade-01.jpg"
-  }])
+  const [comunidades, setComunidades] = React.useState([
+    {
+      id: "1",
+      name: 'Eu odeio acordar cedo',
+      image: "https://alurakut.vercel.app/capa-comunidade-01.jpg",
+      url: "https://www.uol.com.br/vivabem/listas/tem-dificuldade-de-acordar-cedo-entao-veja-9-dicas-que-mudarao-sua-vida.htm"
+    },
+    {
+      id: '2',
+      name: "Brasil Grime Show",
+      image: "../images/brasil.jpg",
+      url: "https://www.youtube.com/channel/UCPuIr0zPwjWcTO4WSEK6hGg"
+    },
+    {
+      id: '3',
+      name: "Nerdologia",
+      image: "../images/nerdologia.jpg",
+      url: "https://www.youtube.com/channel/UClu474HMt895mVxZdlIHXEA"
+    },
+    {
+      id: '4',
+      name: "Rap BR",
+      image: "../images/rapBr.png",
+      url: "https://www.youtube.com/watch?v=OtUgra5BtwI&t=782s"
+    },
+    {
+      id: '5',
+      name: "Alura",
+      image: "https://www.alura.com.br/assets/img/alura-share.1617727198.png",
+      url: "https://www.alura.com.br/"
+    },
+    {
+      id: '6',
+      name: "Investimentos",
+      image: "../images/investimento.jpg",
+      url: "https://www.rico.com.vc/"
+    }
+  ])
 
-  const randomUser = 'fvxstx';
-  const favoritePeople = ["fvxstx", "omariosouto", 'peas', 'juunegreiros']
+  const [favoritePeople, setFavoritePeople] = React.useState([
+    {
+      name:"omariosouto",
+      image:"https://github.com/omariosouto.png",
+      url:"https://github.com/omariosouto"
+    }, 
+    {
+      name:'peas',
+      image:"https://github.com/peas.png",
+      url:"https://github.com/peas"
+    },
+    {
+      name:'juunegreiros', 
+      image:"https://github.com/juunegreiros.png",
+      url:"https://github.com/juunegreiros"
+    },
+    {
+      name:'Irand27',
+      image:"https://github.com/Irand27.png",
+      url:"https://github.com/Irand27"
+    },
+    {
+      name:'brenda1602',
+      image:"https://github.com/brenda1602.png",
+      url:"https://github.com/brenda1602"
+    },
+    {
+      name:'filhodanuvem',
+      image:"https://github.com/filhodanuvem.png",
+      url:"https://github.com/filhodanuvem"
+    },
+    {
+      name:'maykbrito',
+      image:"https://github.com/maykbrito.png",
+      url:"https://github.com/maykbrito"
+    }
+  ])
 
   return (
     <>
@@ -64,7 +137,8 @@ export default function Home() {
               const comunidade = {
                 id: new Date().toISOString(),
                 titulo: dadosDoForm.get('title'),
-                image: dadosDoForm.get('image')
+                image: dadosDoForm.get('image'),
+                url: dadosDoForm.get('url')
               }
 
               const comunidadesAtualizadas = [...comunidades, comunidade]
@@ -88,6 +162,14 @@ export default function Home() {
                 />
               </div>
 
+              <div>
+                <input 
+                  placeholder="Coloque uma URL para sua comunidade"
+                  name="url"
+                  aria-label="Coloque uma URL para sua comunidade"
+                />
+              </div>
+
               <button>
                 Criar comunidade
               </button>
@@ -96,43 +178,9 @@ export default function Home() {
         </div>
         
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-          <ProfileRelationsBoxWrapper>
-            <h2 className="smallTitle">
-              Comunidade ({comunidades.length})
-            </h2>
-
-            <ul>
-              {comunidades.map((comuni) => {
-                return (
-                  <li key={comuni.id}>
-                    <a href={`/users/${comuni.title}`} key={comuni.title}>
-                      <img src={comuni.image}/>
-                      <span>{comuni.title}</span>
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
-          </ProfileRelationsBoxWrapper>
-
-          <ProfileRelationsBoxWrapper>
-            <h2 className="smallTitle">
-              Pessoas da comunidade ({favoritePeople.length})
-            </h2>
-
-            <ul>
-              {favoritePeople.map((favoritePerson) => {
-                return (
-                  <li key={favoritePerson}>
-                    <a href={`/users/${favoritePerson}`} key={favoritePerson}>
-                      <img src={`https://github.com/${favoritePerson}.png`}/>
-                      <span>{favoritePerson}</span>
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
-          </ProfileRelationsBoxWrapper>
+          <LittleRelationBox title="Pessoas da comunidade" arrayProfiles={favoritePeople} />
+            
+          <LittleRelationBox title="Comunidade" arrayProfiles={comunidades}/>
         </div>
       </MainGrid>
     </>
---
+++ src/components/LittleRelationBox/index.js
@@ -0,0 +1,24 @@
+import { ProfileRelationsBoxWrapper } from '../ProfileRelations';
+
+export function LittleRelationBox(props) {
+    return (
+        <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>
+                {props.title} ({props.arrayProfiles.length})
+            </h2>
+
+            <ul>
+                {props.arrayProfiles.slice(0, 6).map((profile) => {
+                    return (
+                        <li key={profile.id ? profile.id : profile.name}>
+                            <a href={profile.url} key={profile.name} target="_blank">
+                                <img src={profile.image} alt={profile.name}/>
+                                <span>{profile.name}</span>
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </ProfileRelationsBoxWrapper>
+    )
+};
